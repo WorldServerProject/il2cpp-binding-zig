a2795e71a3077eed9c63f55505142f9f70973ce3
kenny33690 <kenny33600@gmail.com>
2026-09-20 21:54:23 +0800

    binding generate

--- il2cpp_binding.zig
+++ il2cpp_binding.zig
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9dd70a73a7b0a240493eb8617a971718b47d321d444f8379eed3ff3e504b62f
-size 110560024
+oid sha256:4628d4d6c41a03c107395f0fea6d13afedb82ad2e4d2616396c74161d7adb9bd
+size 110564180
